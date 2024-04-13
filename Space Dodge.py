897634393404485df0595c15bdad08ca38d79f6b
_sobhan_sv <[sv.sobhan@gmail.com]>
2024-04-13 07:36:07 +0430

    PEP8

--- Space Dodge.py
+++ Space Dodge.py
@@ -1,16 +1,15 @@
 import pygame
 import time
 import random
+
 pygame.font.init()
 
-WIDTH, HEIGHT =  1000, 800
+WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space Dodge   <Update : 2>')
 
-
 # upload the location (Space Dodge.jpg) in this section ... :
-BG = pygame.transform.scale(pygame.image.load('...\Space Dodge/bg.jpg'), (WIDTH, HEIGHT))
-
+BG = pygame.transform.scale(pygame.image.load('D:\Sobhan Vaziri\Clone\Space Dodge/bg.jpg'), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
@@ -22,6 +21,7 @@ STAR_HEIGHT = 20
 STAR_VEL = 4
 
 FONT = pygame.font.SysFont('comicsans', 30)
+
 
 def draw(player, elapsed_time, stars):
     WIN.blit(BG, (0, 0))
@@ -35,6 +35,7 @@ def draw(player, elapsed_time, stars):
         pygame.draw.rect(WIN, 'white', star)
 
     pygame.display.update()
+
 
 def main():
     run = True
@@ -63,7 +64,7 @@ def main():
                 stars.append(star)
 
             star_add_increment = max(200, star_add_increment - 50)
-            star_count = 0    
+            star_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -72,7 +73,7 @@ def main():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
-            player.x -= PLAYER_VEL    
+            player.x -= PLAYER_VEL
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
 
@@ -87,15 +88,15 @@ def main():
 
         if hit:
             lost_text = FONT.render('You Lost!! try again', 1, 'white')
-            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))    
+            WIN.blit(lost_text, (WIDTH / 2 - lost_text.get_width() / 2, HEIGHT / 2 - lost_text.get_height() / 2))
             pygame.display.update()
             pygame.time.delay(4000)
             break
 
-
-        draw(player, elapsed_time, stars)    
+        draw(player, elapsed_time, stars)
 
     pygame.quit()
 
+
 if __name__ == '__main__':
-    main()            
+    main()
